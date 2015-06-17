63b9328ec48fd95f391885e1c08e6dcbdcdade3f
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-17 12:49:19 -0400

    added drive-link

--- custom.php
+++ custom.php
@@ -3,7 +3,8 @@
 function YouTube_embed()
 {
 	$src = 'https://www.youtube.com/embed/' . metadata('item', array('Dublin Core', 'Relation'));
-	echo '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '" frameborder="0" allowfullscreen></iframe></div>'; 
+	$href = 'https://drive.google.com/uc?export=download&id=' . metadata('item', array('Dublin Core', 'Identifier'));
+	echo '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '" frameborder="0" allowfullscreen></iframe></div><div class="drive-link"><a href="' . $href . '">Download Original File</a></div>'; 
 }
 
 ; ?>

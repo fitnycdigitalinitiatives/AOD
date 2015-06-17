1ad71ef4440d0612f4ef07d24efa078d6c44dd42
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-17 14:22:45 -0400

    test

--- items/show.php
+++ items/show.php
@@ -17,6 +17,7 @@
 <aside id="sidebar">
 
     <!-- The following returns all of the files associated with an item. -->
+	<?php echo '<div><p>' . getItemType() . '</p></div>'; ?>
     <?php YouTube_embed(); ?>
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->

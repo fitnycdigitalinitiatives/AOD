a1254ab7ecee78d6a7039e5fc219d07e00737b45
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-17 12:40:58 -0400

    typo

--- custom.php
+++ custom.php
@@ -3,7 +3,7 @@
 function YouTube_embed()
 {
 	$src = 'https://www.youtube.com/embed/' . metadata('item', array('Dublin Core', 'Relation'));
-	echo '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '" frameborder="0" allowfullscreen></iframe></div>' 
+	echo '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '" frameborder="0" allowfullscreen></iframe></div>'; 
 }
 
 ; ?>

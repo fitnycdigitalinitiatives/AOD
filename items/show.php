a340421f866c2d25d67960dfff5bac516a9554e4
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-17 15:04:44 -0400

    if video, else has files

--- items/show.php
+++ items/show.php
@@ -3,11 +3,6 @@
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 
 <div id="primary">
-
-    <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
-    <?php echo files_for_item(array('imageSize' => 'fullsize')); ?>
-    <?php endif; ?>
-    
     <?php echo all_element_texts('item'); ?>
     
     <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
@@ -18,7 +13,9 @@
 
     <!-- The following returns all of the files associated with an item. -->
 	<?php if (metadata('item', 'item_type_name') == "Moving Image"): ?>
-    <?php YouTube_embed(); ?>
+    <?php video_embed(); ?>
+	<?php elseif (metadata('item', 'has files')): ?>
+	<?php echo files_for_item(array('imageSize' => 'fullsize')); ?>
 	<?php endif; ?>
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->

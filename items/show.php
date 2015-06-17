508947b94c8cf71050d7460585493d9e7bc89789
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-17 14:44:49 -0400

    test

--- items/show.php
+++ items/show.php
@@ -17,7 +17,7 @@
 <aside id="sidebar">
 
     <!-- The following returns all of the files associated with an item. -->
-	<?php echo metadata('item', 'item_type_name'); ?>
+	<?php echo metadata('item', 'item_type_name', array('no_escape' => true)); ?>
     <?php YouTube_embed(); ?>
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->

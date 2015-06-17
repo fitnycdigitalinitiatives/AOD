8ab03b953938c055785f8e692f76621825047d72
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-17 16:42:04 -0400

    Added new item metadata element

--- custom.php
+++ custom.php
@@ -2,8 +2,8 @@
 
 function video_embed()
 {
-	$src = 'https://www.youtube.com/embed/' . metadata('item', array('Dublin Core', 'Relation'));
-	$href = 'https://drive.google.com/uc?export=download&id=' . metadata('item', array('Dublin Core', 'Identifier'));
+	$src = 'https://www.youtube.com/embed/' . metadata('item', array('Item Type Metadata', 'Identifier.YouTube'));
+	$href = 'https://drive.google.com/uc?export=download&id=' . metadata('item', array('Item Type Metadata', 'Identifier.GoogleDrive'));
 	echo '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '" frameborder="0" allowfullscreen></iframe></div><div class="drive-link"><a href="' . $href . '">Download Original File</a></div>'; 
 }
 

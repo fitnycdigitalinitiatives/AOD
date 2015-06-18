30b342528fce9b8f797e50c3a131f8b385d960a8
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-18 10:51:13 -0400

    removed seasons

--- common/header.php
+++ common/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
+<html lang="<?php echo get_html_lang(); ?>">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">

2451c57f8f9da7447e33d27ad58ef89ec6da597d
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-18 15:03:15 -0400

    building grid

--- items/browse.php
+++ items/browse.php
@@ -28,33 +28,15 @@ $sortLinks[__('Date Added')] = 'added';
 <div class="grid">
 <?php foreach (loop('items') as $item): ?>
 <div class="item tile">
-    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
-    <div class="item-meta">
+
 	<?php if (metadata('item', 'item_type_name') == "Moving Image"): ?>
-	<div class="item-img">
         <?php echo link_to_item(YouTube_thumbnail(), array('class'=>'permalink')); ?>
-    </div>
     <?php elseif (metadata('item', 'has files')): ?>
-    <div class="item-img">
         <?php echo link_to_item(item_image('square_thumbnail')); ?>
-    </div>
-    <?php endif; ?>
-
-    <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-    <div class="item-description">
-        <?php echo $description; ?>
-    </div>
-    <?php endif; ?>
-
-    <?php if (metadata('item', 'has tags')): ?>
-    <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
-        <?php echo tag_string('items'); ?></p>
-    </div>
     <?php endif; ?>
 
     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
-    </div><!-- end class="item-meta" -->
 </div><!-- end class="item tile" -->
 <?php endforeach; ?>
 </div>

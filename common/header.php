1ce39fafb34eb3512adffcdeb4c0d4f90ca957d4
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-06-25 09:22:11 -0400

    Revert "added image"
    This reverts commit a807613c27ab26cb1d0fc72a0010cf4641e637d7.

--- common/header.php
+++ common/header.php
@@ -48,7 +48,7 @@
     <div id="wrap">
         <header role="banner">
             <div id="site-title">
-                <?php echo '<img src="' . img('AODlogo.png') . '" alt="Archive On Demand"'; ?>
+                <?php echo link_to_home_page(theme_logo()); ?>
             </div>
             <div id="search-container" role="search">
                 <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>

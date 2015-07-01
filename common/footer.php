359cc9f61f312a509496aae8b3957e9a39a32f87
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-01 12:48:58 -0400

    less tiles

--- common/footer.php
+++ common/footer.php
@@ -31,7 +31,7 @@ jQuery(document).ready(function () {
 jQuery(document).ready(function($) {
 	$("#featured-carousel").owlCarousel({
 		navigation : true,
-		items : 10, //10 items above 1000px browser width
+		items : 8, //8 items above 1000px browser width
 		itemsDesktop : [1000,5], //5 items between 1000px and 901px
 		itemsDesktopSmall : [900,3], // betweem 900px and 601px
 		itemsTablet: [600,2], //2 items between 600 and 0

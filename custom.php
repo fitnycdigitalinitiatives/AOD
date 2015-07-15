606a0e4f06b91b51e41a823d2ec3ef829c40f957
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-15 17:35:24 -0400

    idiot stuff

--- custom.php
+++ custom.php
@@ -4,7 +4,7 @@ function video_embed()
 {
 	$src = 'https://www.youtube.com/embed/' . metadata('item', array('Item Type Metadata', 'Identifier.YouTube'));
 	$href = 'https://drive.google.com/uc?export=download&id=' . metadata('item', array('Item Type Metadata', 'Identifier.GoogleDrive'));
-	return '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '?rel=0&?autoplay=1&?modestbranding=1&?showinfo=0" frameborder="0" allowfullscreen></iframe></div><div class="drive-link"><a href="' . $href . '">Download Original File (Requires valid fitnyc.edu email)</a></div>'; 
+	return '<div class="videoWrapper"><iframe width="560" height="349" src="' . $src . '?rel=0&autoplay=1&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen></iframe></div><div class="drive-link"><a href="' . $href . '">Download Original File (Requires valid fitnyc.edu email)</a></div>'; 
 }
 
 function YouTube_thumbnail()

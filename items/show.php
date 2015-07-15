746db6829a880a48adeb1ca7d564fdc665739cc1
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-15 15:59:36 -0400

    restructuring show

--- items/show.php
+++ items/show.php
@@ -1,7 +1,13 @@
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
-
-<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
-
+<div class="sub-header">
+	<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+	<nav class="pagination-nav" aria-label="Item-Pagination">
+		<ul class="item-pagination navigation">
+			<li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+			<li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+		</ul>
+	</nav>
+</div>
 <div id="primary">
     <?php echo all_element_texts('item'); ?>
     
@@ -41,10 +47,12 @@
     </div>
 
 </aside>
-
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
-
+<div class="pre-footer">
+	<nav class="pagination-nav" aria-label="Item-Pagination">
+		<ul class="item-pagination navigation">
+			<li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+			<li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+		</ul>
+	</nav>
+</div>
 <?php echo foot(); ?>

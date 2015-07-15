7a4bdbe2e1f73122da75b16a7898b5d9180f4b77
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-15 14:45:24 -0400

    blank search

--- items/browse.php
+++ items/browse.php
@@ -11,40 +11,44 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 
 	<?php if ($total_results > 0): ?>
 
-	<?php
-	$sortLinks[__('Title')] = 'Dublin Core,Title';
-	$sortLinks[__('Creator')] = 'Dublin Core,Creator';
-	$sortLinks[__('Date Added')] = 'added';
-	?>
-	<div id="sort-links">
-		<span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
-	</div>
+		<?php
+		$sortLinks[__('Title')] = 'Dublin Core,Title';
+		$sortLinks[__('Creator')] = 'Dublin Core,Creator';
+		$sortLinks[__('Date Added')] = 'added';
+		?>
+		<div id="sort-links">
+			<span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
+		</div>
 
 	<?php endif; ?>
 </div>
-<div class="grid">
-	<?php foreach (loop('items') as $item): ?>
-	<div class="item tile">
+<?php if ($total_results > 0): ?>
+	<div class="grid">
+		<?php foreach (loop('items') as $item): ?>
+		<div class="item tile">
 
-		<?php if (metadata('item', 'item_type_name') == "Moving Image"): ?>
-			<div class="item-thumb">
-			<?php echo link_to_item(YouTube_thumbnail(), array('class'=>'permalink')); ?>
-			<?php echo link_to_item('<div class="overlay"></div>'); ?>
-			<?php echo link_to_item('<div class="title"><p>' . metadata('item', array('Dublin Core', 'Title')) . '</p></div>'); ?>
-			</div>
-		<?php elseif (metadata('item', 'has files')): ?>
-			<div class="item-thumb">
-			<?php echo link_to_item(item_image('square_thumbnail')); ?>
-			<?php echo link_to_item('<div class="overlay"></div>'); ?>
-			<?php echo link_to_item('<div class="title"><p>' . metadata('item', array('Dublin Core', 'Title')) . '</p></div>'); ?>
-			</div>
-		<?php endif; ?>
+			<?php if (metadata('item', 'item_type_name') == "Moving Image"): ?>
+				<div class="item-thumb">
+				<?php echo link_to_item(YouTube_thumbnail(), array('class'=>'permalink')); ?>
+				<?php echo link_to_item('<div class="overlay"></div>'); ?>
+				<?php echo link_to_item('<div class="title"><p>' . metadata('item', array('Dublin Core', 'Title')) . '</p></div>'); ?>
+				</div>
+			<?php elseif (metadata('item', 'has files')): ?>
+				<div class="item-thumb">
+				<?php echo link_to_item(item_image('square_thumbnail')); ?>
+				<?php echo link_to_item('<div class="overlay"></div>'); ?>
+				<?php echo link_to_item('<div class="title"><p>' . metadata('item', array('Dublin Core', 'Title')) . '</p></div>'); ?>
+				</div>
+			<?php endif; ?>
 
-		<?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+			<?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
-	</div><!-- end class="item tile" -->
-	<?php endforeach; ?>
-</div>
+		</div><!-- end class="item tile" -->
+		<?php endforeach; ?>
+	</div>
+<?php else: ?>
+	<p>Your search returned no results. Please try another keyword. Or try our <a href="/items/search">Advanced Search</a>.</p>
+<?php endif; ?>
 <div class="pre-footer">
 	<?php echo pagination_links(); ?>
 

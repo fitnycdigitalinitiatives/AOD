af7ce4ad75335c8d7605b2db0e6c3981d46b0580
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-15 11:36:42 -0400

    footer

--- common/footer.php
+++ common/footer.php
@@ -2,16 +2,20 @@
 
 <footer role="contentinfo">
 
-        <div id="custom-footer-text">
-            <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
-            <p><?php echo $footerText; ?></p>
-            <?php endif; ?>
-            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-                <p><?php echo $copyright; ?></p>
-            <?php endif; ?>
+        <div id="info">
+            <p id="copyright">©2015  <a href="/">FIT Archive On Demand</a></br>All rights reserved.</p>
+			<p id="address">Seventh Avenue at 27 Street<br>New York City 10001-5992<br>212 217.7999</p>
+			<p id="omeka">Proudly powered by <a href="http://omeka.org">Omeka</a></p>
         </div>
-
-        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+		<div id="social-meda">
+			<span>Archive on Demand is an initiative of the <a href="http://www.fitnyc.edu/library.asp">FIT Library</a>.</span>
+			<ul>
+				<li><a href="https://www.facebook.com/FITLibrary"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://twitter.com/FITLibrary"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="http://pinterest.com/fitlibrary/"><i class="fa fa-pinterest-p"></i></a></li>
+				<li><a href="http://instagram.com/fashioninstitutelibrary"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
 
     <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 

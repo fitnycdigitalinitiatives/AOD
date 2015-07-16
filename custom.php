2344c52bf72cf9682c082f4ce452c3a8efefd27f
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-16 11:03:45 -0400

    related items

--- custom.php
+++ custom.php
@@ -38,13 +38,13 @@ function related_items($current_item)
 		$items = get_records('Item', array('tags' => $tags[0], 'sort_field' => 'random'), 4);
 		set_loop_records('items', $items);
 		if ($items) {
-			$html = '<div class="related_items">';
+			$html = '<div class="related_items"><h2>Related Videos</h2>';
 			foreach (loop('items') as $item) {
 				$src = 'http://img.youtube.com/vi/' . metadata($item, array('Item Type Metadata', 'Identifier.YouTube')) . '/hqdefault.jpg';
 				$img = link_to_item('<img src="' . $src . '">', array('class'=>'permalink'));
 				$overlay = link_to_item('<div class="overlay"></div>', array('class'=>'permalink'));
 				$description = link_to_item('<div class="description"><h3>' . metadata('item', array('Dublin Core', 'Title')) . '</h3></div>', array('class'=>'permalink'));
-				$html .= '<div class="related-item">' . $img . $overlay . '</div>' . $description;
+				$html .= '<div class="related-item"><div class="item-thumb">' . $img . $overlay . '</div>' . $description . '</div>';
 			}
 			$html .= '</div>';
 			return $html;

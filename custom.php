a12c0c35a562b7306c035f5d13c3b594fa86ce69
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-16 12:52:20 -0400

    typo

--- custom.php
+++ custom.php
@@ -43,7 +43,7 @@ function related_items($current_item)
 				$src = 'http://img.youtube.com/vi/' . metadata($item, array('Item Type Metadata', 'Identifier.YouTube')) . '/hqdefault.jpg';
 				$img = link_to_item('<img src="' . $src . '">', array('class'=>'permalink'));
 				$overlay = link_to_item('<div class="overlay"></div>', array('class'=>'permalink'));
-				$description = link_to_item('<div class="description"><h3>' . metadata('item', array('Dublin Core', 'Title')) . '</h3></div>', array('class'=>'permalink'));
+				$description = link_to_item('<div class="description"><h4>' . metadata('item', array('Dublin Core', 'Title')) . '</h4></div>', array('class'=>'permalink'));
 				$html .= '<div class="related-item"><div class="related-thumb">' . $img . $overlay . '</div>' . $description . '</div>';
 			}
 			$html .= '</div>';

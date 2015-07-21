9d03b5793da882cfd3f2efa6ca932e1bc76bc62c
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-21 11:40:34 -0400

    updates

---
+++ common/record-metadata.php
@@ -0,0 +1,13 @@
+<div class="element-set">
+<h2>Metadata</h2>
+<?php foreach ($elementsForDisplay as $setName => $setElements): ?>
+    <?php foreach ($setElements as $elementName => $elementInfo): ?>
+    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+        <h3><?php echo html_escape(__($elementName)); ?></h3>
+        <?php foreach ($elementInfo['texts'] as $text): ?>
+            <div class="element-text"><?php echo $text; ?></div>
+        <?php endforeach; ?>
+    </div><!-- end element -->
+    <?php endforeach; ?>
+<?php endforeach;
+</div><!-- end element-set -->

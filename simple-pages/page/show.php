fe727d2e6a45934274ccedbb37f38b4887c85e89
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-22 17:23:59 -0400

    simple pages

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -3,8 +3,9 @@
     'bodyclass' => 'page simple-page',
     'bodyid' => metadata('simple_pages_page', 'slug')
 )); ?>
-<p id="simple-pages-breadcrumbs" class="navigation secondary-nav"><?php echo simple_pages_display_breadcrumbs(); ?></p>
-<h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
+<div class="sub-header">
+	<h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
+</div>
 <div id="primary">
     <?php
     $text = metadata('simple_pages_page', 'text', array('no_escape' => true));

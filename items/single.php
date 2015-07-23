e1c6d999c844b3e14ce94f99a85b7136e78f9ced
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-23 10:28:10 -0400

    recent items

---
+++ items/single.php
@@ -0,0 +1,5 @@
+<div class="item-link">
+    <?php echo link_to_item('<div class="thumb">' . YouTube_thumbnail() . '<div class="overlay"></div></div><div class="description"><h4>' . metadata('item', array('Dublin Core', 'Title')) . '</h4>
+		</div>', array('class'=>'permalink')); ?>
+</div>
+

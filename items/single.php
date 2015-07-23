9f8273f88a91fd99afdc3a711955940e1d43efa1
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-23 10:44:28 -0400

    test

--- items/single.php
+++ items/single.php
@@ -1,17 +1,4 @@
-<div class="item record">
-    <?php
-    $title = metadata($item, array('Dublin Core', 'Title'));
-    $description = metadata($item, array('Dublin Core', 'Description'), array('snippet' => 150));
-    ?>
-    <h3><?php echo link_to($item, 'show', strip_formatting($title)); ?></h3>
-    <?php if (metadata($item, 'has files')) {
-        echo link_to_item(
-            item_image('square_thumbnail', array(), 0, $item), 
-            array('class' => 'image'), 'show', $item
-        );
-    }
-    ?>
-    <?php if ($description): ?>
-        <p class="item-description"><?php echo $description; ?></p>
-    <?php endif; ?>
+<div class="item-link">
+    <?php echo link_to_item('<div class="thumb">' . YouTube_thumbnail($item) . '<div class="overlay"></div></div><div class="description"><h4>' . metadata($item, array('Dublin Core', 'Title')) . '</h4></div>', array('class'=>'permalink')); ?>
 </div>
+

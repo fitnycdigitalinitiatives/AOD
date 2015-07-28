1ed22497d6d58a7a7b4fdf7c3a123f4a0c4ca818
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-07-28 14:32:24 -0400

    advanced search

---
+++ items/search.php
@@ -0,0 +1,14 @@
+<?php
+$pageTitle = __('Search Items');
+echo head(array('title' => $pageTitle,
+           'bodyclass' => 'items advanced-search'));
+?>
+<div class="sub-header">
+	<h1><?php echo $pageTitle; ?></h1>
+</div>
+<div class="advanced-search">
+	<?php echo $this->partial('items/search-form.php',
+		array('formAttributes' =>
+			array('id'=>'advanced-search-form'))); ?>
+</div>
+<?php echo foot(); ?>

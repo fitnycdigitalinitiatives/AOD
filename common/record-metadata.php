695a143566e08f1b736498037dbaaa7e33bbdbdc
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-10-27 12:43:18 -0400

    testing

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -5,7 +5,7 @@
     <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
         <h3><?php echo html_escape(__($elementName)); ?></h3>
         <?php foreach ($elementInfo['texts'] as $text): ?>
-            <div class="element-text"><?php echo $text; ?></div>
+            <div class="element-text"><?php echo $elementInfo['id']; ?></div>
         <?php endforeach; ?>
     </div><!-- end element -->
     <?php endforeach; ?>

4848bc83eca69029930ddf61578d07f89a0ef323
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-10-27 15:50:18 -0400

    building

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -6,7 +6,7 @@
 		<div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
 			<h3><?php echo html_escape(__($elementName)); ?></h3>
 			<?php foreach ($elementInfo['texts'] as $text): ?>
-				<div class="element-text"><?php echo $text; ?></div>
+				<div class="element-text"><?php echo heading_links($elementName, $text); ?></div>
 			<?php endforeach; ?>
 		</div><!-- end element -->
 	<?php else: ?>

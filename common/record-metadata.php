b8c966eb5de1cfe334a7835edf0db204e0871f4e
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-10-27 12:49:16 -0400

    adding hyperlinks to elements

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -1,13 +1,33 @@
 <div class="element-set">
 <h2>Metadata</h2>
 <?php foreach ($elementsForDisplay as $setName => $setElements): ?>
-    <?php foreach ($setElements as $elementName => $elementInfo): ?>
-    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
-        <h3><?php echo html_escape(__($elementName)); ?></h3>
-        <?php foreach ($elementInfo['id'] as $id): ?>
-            <div class="element-text"><?php echo $id; ?></div>
-        <?php endforeach; ?>
-    </div><!-- end element -->
-    <?php endforeach; ?>
+	<?php foreach ($setElements as $elementName => $elementInfo): ?>
+		<?php if ($elementName == "Subject"): ?>
+			<?php $element = get_db()->getTable('Element')->findByElementSetNameAndElementName('Dublin Core', $elementName); ?>
+			<?php $id = $element->id; ?>
+			<div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+				<h3><?php echo html_escape(__($elementName)); ?></h3>
+				<?php foreach ($elementInfo['texts'] as $text): ?>
+					<div class="element-text"><?php echo link_to_items_browse($text, array('element_id' => $id, 'terms' => $term, 'type' => 'is exactly')); ?></div>
+				<?php endforeach; ?>
+			</div><!-- end element -->
+		<?php else: ?>
+			<div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+				<h3><?php echo html_escape(__($elementName)); ?></h3>
+				<?php foreach ($elementInfo['texts'] as $text): ?>
+					<div class="element-text"><?php echo $text; ?></div>
+				<?php endforeach; ?>
+			</div><!-- end element -->
+		<?php endif; ?>
+	<?php endforeach; ?>
 <?php endforeach; ?>
 </div><!-- end element-set -->
+
+
+
+
+link_to_items_browse($text, array('element_id' => $id, 'terms' => $term, 'type' => 'is exactly'))
+
+
+$element = get_db()->getTable('Element')->findByElementSetNameAndElementName('Dublin Core', $elementName);
+$id = $element->id;

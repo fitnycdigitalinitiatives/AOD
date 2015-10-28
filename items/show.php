2d28ed9a49aca0f222d40ccd4f7e4db76200259f
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-10-28 12:48:44 -0400

    typo

--- items/show.php
+++ items/show.php
@@ -58,7 +58,7 @@
 			<h2><?php echo __('Related Parts'); ?></h2>
 			<?php $relation = metadata('item', array('Dublin Core', 'Relation'), array ('all' => true)) ?>
 			<?php foreach ($relation as $part): ?>
-			<?php $part_item = get_record('Item', array('advanced' => array(array('element_id' => '50', 'type' => 'is exactly', 'terms' => $part))));>
+			<?php $part_item = get_record('Item', array('advanced' => array(array('element_id' => '50', 'type' => 'is exactly', 'terms' => $part)))); ?>
 			<div class="element-text">
 				<?php echo link_to_item($part, 'show', $part_item); ?>
 			</div>

b831c6fa96d3d564e2e518edcb458f327eb4a8ff
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-11-04 17:00:14 -0500

    video count

--- items/tags.php
+++ items/tags.php
@@ -13,7 +13,7 @@ echo head(array('title'=>$pageTitle, 'bodyclass'=>'items tags'));
 		<?php $item = get_records('Item', array('tags' => $tag['name']), 1); ?>
 		<?php echo link_to_items_browse(YouTube_thumbnail($item[0]), array('tags' => $tag['name'])); ?>
 		<?php echo link_to_items_browse('<div class="overlay"></div>', array('tags' => $tag['name'])); ?>
-		<?php echo link_to_items_browse('<div class="title"><h2>' . $tag['name'] . '</h2></div>', array('tags' => $tag['name'])); ?>
+		<?php echo link_to_items_browse('<div class="title"><h2>' . $tag['name'] . '</h2><div class="video-count">' . $tag["tagCount"] . '</div></div>', array('tags' => $tag['name'])); ?>
 	</div>
 </div>
 <?php endforeach; ?>

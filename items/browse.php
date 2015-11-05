728e6d881114194c480d3326698b9478185706ae
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-11-05 11:51:39 -0500

    extent

--- items/browse.php
+++ items/browse.php
@@ -31,7 +31,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 				<div class="item-thumb">
 				<?php echo link_to_item(YouTube_thumbnail($item), array('class'=>'permalink')); ?>
 				<?php echo link_to_item('<div class="overlay"></div>'); ?>
-				<?php echo link_to_item('<div class="title"><p>' . metadata('item', array('Dublin Core', 'Title')) . '</p></div><div class="extent">' . metadata('item', array('Dublin Core', 'Extent')) . '</div>'); ?>
+				<?php echo link_to_item('<div class="title"><p>' . metadata('item', array('Dublin Core', 'Title')) . '</p></div><div class="extent"><i class="fa fa-clock-o"></i> ' . metadata('item', array('Dublin Core', 'Extent')) . '</div>'); ?>
 				</div>
 			<?php elseif (metadata('item', 'has files')): ?>
 				<div class="item-thumb">

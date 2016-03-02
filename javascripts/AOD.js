ff9ab2b1c43d2a73760d14bd191a505bdcdbb4d1
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-03-02 17:58:26 -0500

    mobile

--- javascripts/AOD.js
+++ javascripts/AOD.js
@@ -7,7 +7,7 @@ if (!AOD) {
         var dropdownMenu = $('#site-title');
         dropdownMenu.append('<a href="#" class="menu"><i class="fa fa-bars  fa-3x"></i></a>');
         //Hide the rest of the menu
-        $('#mobile-nav .navigation').hide();
+        $('#mobile-nav').hide();
 
         //function the will toggle the menu
         $('.menu').click(function() {

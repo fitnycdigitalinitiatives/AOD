09fdfdcfb14273b5c9ebf3d9e3521131f9765921
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-03-02 17:56:54 -0500

    mobile

--- javascripts/AOD.js
+++ javascripts/AOD.js
@@ -4,14 +4,14 @@ if (!AOD) {
 
 (function ($) {    
     AOD.dropDown = function(){
-        var dropdownMenu = $('#mobile-nav');
-        dropdownMenu.prepend('<a href="#" class="menu"><i class="fa fa-bars  fa-3x"></i></a>');
+        var dropdownMenu = $('#site-title');
+        dropdownMenu.append('<a href="#" class="menu"><i class="fa fa-bars  fa-3x"></i></a>');
         //Hide the rest of the menu
         $('#mobile-nav .navigation').hide();
 
         //function the will toggle the menu
         $('.menu').click(function() {
-            $("#mobile-nav .navigation").slideToggle();
+            $("#mobile-nav").slideToggle();
         });
     };
 })(jQuery)

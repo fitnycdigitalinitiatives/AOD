ccd9fe391b37db175e418798df2c7fe4f7e8e7be
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-03-22 15:28:07 -0400

    changed some dublin core labels

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -1,17 +1,39 @@
 <div class="element-set">
-<h2>Metadata</h2>
+<h2>About This Video</h2>
 <?php foreach ($elementsForDisplay as $setName => $setElements): ?>
     <?php foreach ($setElements as $elementName => $elementInfo): ?>
+	<?php
+		switch($elementName) {
+			// Check if $elementName is 'Extent'
+			case 'Extent':
+				$elementLabel = 'Length';
+				break;
+
+			// Check if $elementName is 'Temporal Coverage'
+			case 'Temporal Coverage':
+				$elementLabel = 'Time Period';
+				break;
+				
+			// Check if $elementName is 'Spatial Coverage'
+			case 'Spatial Coverage':
+				$elementLabel = 'Geography';
+				break;
+
+			// Leave the default for headings you don't want to change.
+			default:
+				$elementLabel = $elementName;
+		}
+	; ?>
 	<?php if (($setName == "Dublin Core") and (($elementName == "Subject") or ($elementName == "Contributor") or ($elementName == "Spatial Coverage") or ($elementName == "Temporal Coverage"))): ?>
 		<div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
-			<h3><?php echo html_escape(__($elementName)); ?></h3>
+			<h3><?php echo html_escape(__($elementLabel)); ?></h3>
 			<?php foreach ($elementInfo['texts'] as $text): ?>
 				<div class="element-text"><?php echo heading_links($elementName, $text); ?></div>
 			<?php endforeach; ?>
 		</div><!-- end element -->
 	<?php else: ?>
 		<div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
-			<h3><?php echo html_escape(__($elementName)); ?></h3>
+			<h3><?php echo html_escape(__($elementLabel)); ?></h3>
 			<?php foreach ($elementInfo['texts'] as $text): ?>
 				<div class="element-text"><?php echo $text; ?></div>
 			<?php endforeach; ?>

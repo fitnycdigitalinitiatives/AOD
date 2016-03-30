bea404c5eaae58c8c222691fbd3cac448c9bae38
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-03-30 18:04:59 -0400

    collection shortcode

--- collections/single.php
+++ collections/single.php
@@ -5,8 +5,10 @@
 	$item = get_records('Item', array('collection' => metadata($collection, 'id')), 1);
 	$collectionImage = YouTube_thumbnail($item[0])
     ?>
-	<div class="collection-link-thumb">
-		<?php echo link_to_items_browse($collectionImage, array('collection' => metadata($collection, 'id'))); ?>
+	<div class="collection-link">
+		<div class="collection-link-thumb">
+			<?php echo link_to_items_browse($collectionImage, array('collection' => metadata($collection, 'id'))); ?>
+		</div>
 		<div class="description">
 			<h4><?php echo link_to_items_browse($title, array('collection' => metadata($collection, 'id'))); ?></h4>
 			<p><?php echo $description; ?></p>

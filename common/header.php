dfabe4db68d5a0adac1e4160585303eae37c0a0d
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-04-07 12:21:12 -0400

    google analytics

--- common/header.php
+++ common/header.php
@@ -44,6 +44,17 @@
     ?>
 
     <?php echo head_js(); ?>
+	<!-- Google Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-76102248-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>

e6811a7a049506b78cea28d4df6633a299c22d52
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-04-07 12:00:36 -0400

    addthis script

--- items/show.php
+++ items/show.php
@@ -129,5 +129,6 @@
 			});
 </script>
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55c8b7772f2446eb" async="async"></script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55c8b7772f2446eb"></script>
+
 <?php echo foot(); ?>

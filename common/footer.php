4028a6d49e8f9c4523f9d2fc682480d0280ad3a2
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-04-14 16:09:39 -0400

    footer

--- common/footer.php
+++ common/footer.php
@@ -3,12 +3,13 @@
 <footer role="contentinfo">
 
         <div id="info">
-            <p id="copyright">©2016 <a href="/">FIT Archive On Demand</a></br>All rights reserved.</p>
+            <p id="copyright">©2016 Fashion Institute of Technology</br>All rights reserved.</p>
 			<p id="address">Seventh Avenue at 27 Street<br>New York City 10001-5992<br>212 217.7999</p>
         </div>
 		<div id="social-media">
-			<p>Archive on Demand is an initiative of the <a href="http://www.fitnyc.edu/library.asp">FIT Library</a></p>
+			<p>Archive on Demand is an initiative of the FIT Library</p>
 			<ul>
+				<li><a href="http://www.fitnyc.edu/library/"><i class="fa fa-home fa-3x"></i></a></li>
 				<li><a href="https://www.facebook.com/FITLibrary"><i class="fa fa-facebook fa-3x"></i></a></li>
 				<li><a href="https://twitter.com/FITLibrary"><i class="fa fa-twitter fa-3x"></i></a></li>
 				<li><a href="http://pinterest.com/fitlibrary/"><i class="fa fa-pinterest-p fa-3x"></i></a></li>

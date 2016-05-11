56e525175f00891a2a589a15b01d58d66b1d7ada
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-05-11 11:18:35 -0400

    delay add this

--- common/footer.php
+++ common/footer.php
@@ -31,6 +31,10 @@ jQuery(document).ready(function () {
     AOD.dropDown();
 });
 </script>
+<?php if (@$bodyclass == "items show"): ?>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55c8b7772f2446eb"></script>
+<?php endif; ?>
 
 </body>
 

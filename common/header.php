49f5bef8e0529d2a4ee545f28e90fadde88f416c
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-10-18 17:05:17 -0400

    updating search filter

--- common/header.php
+++ common/header.php
@@ -67,8 +67,10 @@
 			<div id="menu-button">
 				<a href="#" class="menu"><i class="fa fa-bars  fa-2x"></i></a>
 			</div>
-            <div id="search-container" class="desktop" role="search">
-				<form id="search-form" name="search-form" action="<?php echo url('/items/browse'); ?>" method="get">
+      <div id="search-container" class="desktop" role="search">
+        <!-- Get filter for plugin use -->
+      	<?php $url = apply_filters('search_form_default_action', url('/items/browse')); ?>
+				<form id="search-form" name="search-form" action="<?php echo $url; ?>" method="get">
 					<input type="text" name="search" id="keyword-search" value="" size="40" title="Search">
 					<button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
 				</form>

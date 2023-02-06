aab84860cb5b23ddf7efb7831162f7d4602c82b5
Joseph Anderson <joseph_anderson@fitnyc.edu>
2023-02-06 13:56:07 -0500

    changed analytics tag

--- common/header.php
+++ common/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="<?php echo get_html_lang(); ?>">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php echo social_tags(@$bodyclass); ?>
+    <?php echo social_tags(@$bodyclass); ?>
 
     <?php
     if (isset($title)) {
@@ -11,16 +12,18 @@
     }
     $titleParts[] = option('site_title');
     ?>
-    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+    <title>
+        <?php echo implode(' &middot; ', $titleParts); ?>
+    </title>
 
     <?php echo auto_discovery_link_tags(); ?>
 
     <!-- Plugin Stuff -->
-    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+    <?php fire_plugin_hook('public_head', array('view' => $this)); ?>
 
-	<!-- Icon -->
-  <link rel="icon" href="<?php echo img('favicon.ico'); ?>" type="image/x-icon">
-  <link rel="shortcut icon" href="<?php echo img('favicon.ico'); ?>" type="image/x-icon">
+    <!-- Icon -->
+    <link rel="icon" href="<?php echo img('favicon.ico'); ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo img('favicon.ico'); ?>" type="image/x-icon">
 
     <!-- Stylesheets -->
     <?php
@@ -32,65 +35,67 @@
 
     <!-- JavaScripts -->
     <?php
-    queue_js_file(array(
-        'vendor/selectivizr',
-        'vendor/jquery-accessibleMegaMenu',
-        'vendor/respond',
-        'jquery-extra-selectors',
-        'AOD',
-        'globals',
-		'owl.carousel.min'
-    ));
+    queue_js_file(
+        array(
+            'vendor/selectivizr',
+            'vendor/jquery-accessibleMegaMenu',
+            'vendor/respond',
+            'jquery-extra-selectors',
+            'AOD',
+            'globals',
+            'owl.carousel.min'
+        )
+    );
     ?>
 
     <?php echo head_js(); ?>
-	<!-- Google Analytics -->
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1DZXNGW2KT"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
 
-	  ga('create', 'UA-76102248-1', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
+        gtag('config', 'G-1DZXNGW2KT');
+    </script>
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
-    <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
-    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
-    <div id="wrap">
-        <header role="banner">
-            <div id="site-title">
-                <?php echo link_to_home_page('<img src="' . img('aod_brand.png') . '" alt="FIT Archive On Demand">'); ?>
-            </div>
-			<div id="menu-button">
-				<a href="#" class="menu"><i class="fa fa-bars  fa-2x"></i></a>
-			</div>
-            <div id="search-container" class="desktop" role="search">
-				<form id="search-form" name="search-form" action="<?php echo url('/items/browse'); ?>" method="get">
-					<input type="text" name="search" id="keyword-search" value="" size="40" title="Search">
-					<button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
-				</form>
-			</div>
-			<nav id="top-nav" class="top" role="navigation">
+<a href="#content" id="skipnav">
+    <?php echo __('Skip to main content'); ?>
+</a>
+<?php fire_plugin_hook('public_body', array('view' => $this)); ?>
+<div id="wrap">
+    <header role="banner">
+        <div id="site-title">
+            <?php echo link_to_home_page('<img src="' . img('aod_brand.png') . '" alt="FIT Archive On Demand">'); ?>
+        </div>
+        <div id="menu-button">
+            <a href="#" class="menu"><i class="fa fa-bars  fa-2x"></i></a>
+        </div>
+        <div id="search-container" class="desktop" role="search">
+            <form id="search-form" name="search-form" action="<?php echo url('/items/browse'); ?>" method="get">
+                <input type="text" name="search" id="keyword-search" value="" size="40" title="Search">
+                <button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
+            </form>
+        </div>
+        <nav id="top-nav" class="top" role="navigation">
             <?php echo public_nav_main(); ?>
-			</nav>
+        </nav>
 
-            <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
-        </header>
-		<div id="mobile-nav" role="navigation" aria-label="<?php echo __('Mobile Navigation'); ?>">
-			<?php echo public_nav_main(); ?>
-			<div id="search-container" class="mobile" role="search">
-				<form id="search-form" name="search-form" action="<?php echo url('/items/browse'); ?>" method="get">
-					<input type="text" name="search" id="keyword-search" value="" size="40" title="Search">
-					<button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
-				</form>
-			</div>
-		</div>
-        <div id="content" role="main" tabindex="-1">
-            <?php
-                if(! is_current_url(WEB_ROOT)) {
-                  fire_plugin_hook('public_content_top', array('view'=>$this));
-                }
-            ?>
+        <?php fire_plugin_hook('public_header', array('view' => $this)); ?>
+    </header>
+    <div id="mobile-nav" role="navigation" aria-label="<?php echo __('Mobile Navigation'); ?>">
+        <?php echo public_nav_main(); ?>
+        <div id="search-container" class="mobile" role="search">
+            <form id="search-form" name="search-form" action="<?php echo url('/items/browse'); ?>" method="get">
+                <input type="text" name="search" id="keyword-search" value="" size="40" title="Search">
+                <button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
+            </form>
+        </div>
+    </div>
+    <div id="content" role="main" tabindex="-1">
+        <?php
+        if (!is_current_url(WEB_ROOT)) {
+            fire_plugin_hook('public_content_top', array('view' => $this));
+        }
+        ?>
